ae9d7c153cf0d5639a7b59db0bfcab78d0e6dae9
Peter <peter@peters-iMac.local>
2014-01-24 15:30:58 +0800

    Initial Commit

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by Peter on 24/1/14.
+//  Copyright (c) 2014 iCare4u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGitHubTests/TestGitHubTests.m
@@ -0,0 +1,34 @@
+//
+//  TestGitHubTests.m
+//  TestGitHubTests
+//
+//  Created by Peter on 24/1/14.
+//  Copyright (c) 2014 iCare4u. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestGitHubTests : XCTestCase
+
+@end
+
+@implementation TestGitHubTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
